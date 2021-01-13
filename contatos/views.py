ce3c1dd287cc7b992bcdd1bdd58c424a1ad4177a
kaiquepill <kaique.gomes@bandtec.com.br>
2021-01-13 12:51:11 -0300

    view com dados dinamicos

--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Contato
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    contatos = Contato.objects.all()
+    return render(request, 'index.html', {
+        'contatos': contatos
+    })
